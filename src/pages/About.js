f6d90d7fbf6a711b4516a952cbce8d1f6a44bad0
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-03-11 15:08:08 +0000

    Added About page

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Welcome to Joe's Cheese Shop, your go-to destination for all things cheese! Founded in 2021 by Joe, a passionate cheese enthusiast, our shop is dedicated to bringing you the finest selection of cheeses from around the globe.</p>
+            <p>Joe's journey into the world of cheese began during his travels across Europe, where he discovered the rich and diverse flavors of artisanal cheeses. Inspired by the craftsmanship and tradition behind cheese-making, Joe decided to turn his passion into a profession.</p>
+            <p>At Joe's Cheese Shop, we believe that cheese is more than just food; it's an experience. That's why we offer a wide range of services, including cheese tasting sessions, pairing workshops, and personalized cheese boards. Whether you're a cheese novice or a connoisseur, we're here to help you explore and enjoy the wonderful world of cheese. </p>
+            <p>Thank you for choosing Joe's Cheese Shop. We look forward to sharing our love of cheese with you!</p>
+        </div>
+    );
+}
+
+export default About;
